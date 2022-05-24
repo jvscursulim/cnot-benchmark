0b4a5600bb898c0e6f3f25a2b80e4d4031f762e4
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-05-24 05:19:21 -0300

    Updated benchmark.py and new data from experiments

--- benchmark.py
+++ benchmark.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 from cnot import controlled_rx_cnot, hadamard_cz_cnot, molmer_sorensen_cnot, standard_cnot, sqrt_xx_cnot, sqrt_swap_cnot
 from experiments import counts_experiment, process_result, process_infidelity_experiment, avg_gate_infidelity_experiment, cnot_truth_table_experiment
 from qiskit.providers.aer.noise import depolarizing_error, pauli_error
-from plot import plot_code_experiments_results, plot_infidelity_experiments_results
+from plot import plot_experiments_results
 from qec import create_3_repetition_code_circuit
 
 date_and_time = str(dt.datetime.now()).replace(":","-").split(".")[0].replace(" ", "_")
@@ -64,7 +64,7 @@ df_data_truth_table_experiment = pd.DataFrame(data = data_truth_table_experiment
 df_data_truth_table_experiment.to_csv("results/checking_truth_table_experiment.csv")
 
 columns = ["Error probability", "00 Counts", "01 Counts", "10 Counts", "11 Counts", "p_00", "p_not_00", "Process infidelity", "Average gate infidelity"]
-columns_qec = ["Error probability", "p_success", "p_logical_error"]
+columns_qec = ["Error probability", "Process infidelity", "Average gate infidelity", "p_success", "p_logical_error"]
 type_of_errors_list = ["bitflip", "depolarizing", "bitflip_and_coherent_errors", "depolarizing_and_coherent_errors"]
 
 errors_list = sorted([0.0, 0.1, 0.075, 0.05, 0.025, 0.01, 0.0075, 0.005, 0.0025, 0.001, 0.00075, 0.0005, 0.00025, 0.0001])
@@ -102,8 +102,8 @@ for cnot in cnots_list:
                     continue
                 else:
                     
-                    qc_with_meas_bf = standard_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = standard_cnot(noisy = cx_error)
+                    qc_with_meas = standard_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = standard_cnot(noisy = cx_error)
             elif cnot == "hadamard_cz":
                 
                 if error == "bitflip_and_coherent_errors" or error == "depolarizing_and_coherent_errors":
@@ -111,8 +111,8 @@ for cnot in cnots_list:
                     continue
                 else:
                     
-                    qc_with_meas_bf = hadamard_cz_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = hadamard_cz_cnot(noisy = cx_error)
+                    qc_with_meas = hadamard_cz_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = hadamard_cz_cnot(noisy = cx_error)
             elif cnot == "sqrt_xx":
                 
                 if error == "bitflip_and_coherent_errors" or error == "depolarizing_and_coherent_errors":
@@ -120,49 +120,47 @@ for cnot in cnots_list:
                     continue
                 else:
                     
-                    qc_with_meas_bf = sqrt_xx_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = sqrt_xx_cnot(noisy = cx_error)
+                    qc_with_meas = sqrt_xx_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = sqrt_xx_cnot(noisy = cx_error)
             elif cnot == "controlled_rx":
                 
                 if error == "bitflip" or error == "depolarizing":
                     
-                    qc_with_meas_bf = controlled_rx_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = controlled_rx_cnot(noisy = cx_error)
+                    qc_with_meas = controlled_rx_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = controlled_rx_cnot(noisy = cx_error)
                 else:
                     
-                    qc_with_meas_bf = controlled_rx_cnot(measurements = True, noisy = cx_error, params = params_list[0])
-                    qc_without_meas_bf = controlled_rx_cnot(noisy = cx_error, params = params_list[0])
+                    qc_with_meas = controlled_rx_cnot(measurements = True, noisy = cx_error, params = params_list[0])
+                    qc_without_meas = controlled_rx_cnot(noisy = cx_error, params = params_list[0])
             elif cnot == "molmer_sorensen":
                 
                 if error == "bitflip" or error == "depolarizing":
                     
-                    qc_with_meas_bf = molmer_sorensen_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = molmer_sorensen_cnot(noisy = cx_error)
+                    qc_with_meas = molmer_sorensen_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = molmer_sorensen_cnot(noisy = cx_error)
                 else:
                     
-                    qc_with_meas_bf = molmer_sorensen_cnot(measurements = True, noisy = cx_error, params = params_list[1])
-                    qc_without_meas_bf = molmer_sorensen_cnot(noisy = cx_error, params = params_list[1])
+                    qc_with_meas = molmer_sorensen_cnot(measurements = True, noisy = cx_error, params = params_list[1])
+                    qc_without_meas = molmer_sorensen_cnot(noisy = cx_error, params = params_list[1])
             else:
                 
                 if error == "bitflip" or error == "depolarizing":
                     
-                    qc_with_meas_bf = sqrt_swap_cnot(measurements = True, noisy = cx_error)
-                    qc_without_meas_bf = sqrt_swap_cnot(noisy = cx_error)
+                    qc_with_meas = sqrt_swap_cnot(measurements = True, noisy = cx_error)
+                    qc_without_meas = sqrt_swap_cnot(noisy = cx_error)
                 else:
                     
-                    qc_with_meas_bf = sqrt_swap_cnot(measurements = True, noisy = cx_error, params = params_list[2])
-                    qc_without_meas_bf = sqrt_swap_cnot(noisy = cx_error, params = params_list[2])
+                    qc_with_meas = sqrt_swap_cnot(measurements = True, noisy = cx_error, params = params_list[2])
+                    qc_without_meas = sqrt_swap_cnot(noisy = cx_error, params = params_list[2])
                     
-                
+            qc_code = create_3_repetition_code_circuit(env_param = np.pi/2, noisy_cnot = qc_without_meas)
             
-            qc_code_bf = create_3_repetition_code_circuit(env_param = np.pi/2, noisy_cnot = qc_without_meas_bf)
-            
-            counts = counts_experiment(circuit = qc_with_meas_bf, shots = 8192)
-            counts_code_bf = counts_experiment(circuit = qc_code_bf, shots = 8192)
-            processed_counts = process_result(code_qubits_register = qc_with_meas_bf.qregs[0], logical_bit = "0", counts = counts, num_shots = 8192)
-            processed_counts_bf = process_result(code_qubits_register = qc_code_bf.qregs[1], logical_bit = "0", counts = counts_code_bf, num_shots = 8192, has_ancilla = True)
-            process_infidelity = process_infidelity_experiment(circuit = qc_without_meas_bf)
-            avg_gate_infidelity = avg_gate_infidelity_experiment(circuit = qc_without_meas_bf)
+            counts = counts_experiment(circuit = qc_with_meas, shots = 8192)
+            counts_code = counts_experiment(circuit = qc_code, shots = 8192)
+            processed_counts = process_result(code_qubits_register = qc_with_meas.qregs[0], logical_bit = "0", counts = counts, num_shots = 8192)
+            processed_counts_code = process_result(code_qubits_register = qc_code.qregs[1], logical_bit = "0", counts = counts_code, num_shots = 8192, has_ancilla = True)
+            process_infidelity = process_infidelity_experiment(circuit = qc_without_meas)
+            avg_gate_infidelity = avg_gate_infidelity_experiment(circuit = qc_without_meas)
                     
             if '00' in counts.keys():
                         
@@ -190,7 +188,7 @@ for cnot in cnots_list:
                 counts_11 = 0
                         
             data_cnot_infidelity.append([p_error, counts_00, counts_01, counts_10, counts_11, processed_counts[0], processed_counts[1], process_infidelity, avg_gate_infidelity])
-            data_qec_cnot_infidelity.append([p_error, processed_counts_bf[0], processed_counts_bf[1]])
+            data_qec_cnot_infidelity.append([p_error, process_infidelity, avg_gate_infidelity, processed_counts_code[0], processed_counts_code[1]])
         
         if len(data_cnot_infidelity) != 0 and len(data_qec_cnot_infidelity) != 0:
                         
@@ -199,6 +197,6 @@ for cnot in cnots_list:
             df_data_qec_cnot_infidelity = pd.DataFrame(data = data_qec_cnot_infidelity, columns = columns_qec)
             df_data_qec_cnot_infidelity.to_csv(f"results/qec_313_{error}_{cnot}_cnot_{date_and_time}.csv")
             
-            plot_infidelity_experiments_results(data = df_data_cnot_infidelity, save_fig = True, fig_name = f"{error}_{cnot}_cnot_{date_and_time}")
-            plot_code_experiments_results(data1 = df_data_cnot_infidelity, data2 = df_data_qec_cnot_infidelity, save_fig = True, fig_name = f"qec_313_{error}_{cnot}_cnot_{date_and_time}")
+            plot_experiments_results(data = df_data_cnot_infidelity, type_data = "infidelity", save_fig = True, fig_name = f"{error}_{cnot}_cnot_{date_and_time}")
+            plot_experiments_results(data = df_data_qec_cnot_infidelity, type_data = "qec", save_fig = True, fig_name = f"qec_313_{error}_{cnot}_cnot_{date_and_time}")
         
